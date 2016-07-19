af801510cb7cef3174dc2f16164f3146d376868d
ryan <ryan.reasoner@gmail.com>
2016-07-19 15:26:54 -0600

    update

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -1,16 +1,46 @@
+//Designed to be a terminal based calculator
 public class Calculator
 {
+	//main method
 	public static void main(String[] args)
 	{
+		//run tests to make sure everything works
 		runTests();
+
+		//run application
 		run();
 	}
 
+	//run tests
 	private static void runTests()
 	{
 	}
 
+	//run application
 	private static void run()
 	{
+	}
+}
+
+//designed to parse console input
+public class Parser
+{
+	private String ivnalidCharactersRegex = "[]";
+	public static void parseInput(String input)
+	{
+		if(validate(input))
+		{
+			
+		}
+		else
+		{
+			System.out.println("Invalid Input");
+		}
+	}
+
+	//use a regex to parse for valid characters
+	private static boolean validate(String input)
+	{
+		return true;
 	}
 }
