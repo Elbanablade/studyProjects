da258f323b4748a3e2d21bda9adfbc7d3cdb61bf
ryan <ryan.reasoner@gmail.com>
2016-07-19 08:39:34 -0600

    initial commit

---
+++ calculator/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator
+{
+	public static void main(String[] args)
+	{
+		runTests();
+		run();
+	}
+
+	private static void runTests()
+	{
+	}
+
+	private static void run()
+	{
+	}
+}
